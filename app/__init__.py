09203bc6eeb50bfacc14ed4580b6a440bd0be4c0
tamanna-dhir <tamannadhir05@gmail.com>
2025-11-19 03:42:51 +0530

    added gif

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
 # app/__init__.py
 from flask import Flask
+import os
 
 def create_app():
     """
     Factory function to create and configure the Flask application.
     """
-    app = Flask(__name__)
+    app = Flask(
+        __name__,
+        static_folder='static',
+        template_folder='templates'
+    )
 
     # ✅ Import and register blueprints
     from app.routes import main
